107e2790c7c2fcbf47e76517620024580ef9c8ce
toha827 <tohakktl@gmail.com>
2019-07-06 18:43:20 +0600

    hh

--- app/src/main/java/com/example/myapplication/ProfileActivity.kt
+++ app/src/main/java/com/example/myapplication/ProfileActivity.kt
@@ -7,14 +7,16 @@ import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.view.animation.AnimationUtils
+import com.example.myinstagram.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : BaseActivity(4) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val TAG = "ProfileActivity"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        setupBottomNavigation()
         Log.e(TAG,"")
     }
 }
