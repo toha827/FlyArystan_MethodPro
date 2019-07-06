950be0cd0fea765c5ae4638ffe4b0d1d5408f8c9
toha827 <tohakktl@gmail.com>
2019-07-06 16:18:36 +0600

    Logo

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -12,19 +12,7 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
 
-
-
-        ic_logo.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_in))
-
-        Handler().postDelayed({
-            ic_logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.splash_out))
-            Handler().postDelayed({
-                ic_logo.visibility = View.GONE
-                startActivity(Intent(this,DashBoardActivity::class.java))
-                finish()
-            },500)
-        },1500)
     }
 }
